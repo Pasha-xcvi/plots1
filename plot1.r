bfbd5830181536f1a6a13dcd4228421679024eb4
Pasha-xcvi <60955195+Pasha-xcvi@users.noreply.github.com>
2020-04-18 02:55:17 +0300

    Add files via upload

---
+++ plot1.r
@@ -0,0 +1,31 @@
+#dataset is imported thtough File->import dataset
+#Reformatting of the date field in the dataset
+household_power_consumption[,"Date"] <- as.Date(househould_power_consumption[,"Date"],format = "%d/%m/%Y")
+
+
+
+
+#create a subset for the selected timeline between 01-02 and 02-02 in 2007
+mysub <- subset(data,Date == "2007-02-01" | Date == "2007-02-02")
+
+
+
+
+#convert the filed of global active power from string of characters to numeric field
+gap<-as.numeric(mysub$Global_active_power)
+
+
+#define the desired graph format to be saved along with its dimensions 
+
+
+png("plot1.png",width = 480, height = 480)
+
+
+#create a histogram plot for global active power along with desired characteristics
+hist(gap, col = "red", main = "Global Active Power",
+
+
+xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+#save the graph
+
+dev.off()
